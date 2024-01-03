cb28e08b7ab73f5b8b11bdf4e805351d919d7d15
RounakBytes <144313548+RounakBytes@users.noreply.github.com>
2024-01-03 16:08:40 +0530

    Area Bug Fixed

--- converter.js
+++ converter.js
@@ -21,24 +21,25 @@ export function convertTemperature(fromValue, fromUnit, toUnit) {
 }
 
 export function convertArea(value, fromUnit, toUnit) {
-    const conversions = {
-      squareMeters: {
-        squareFeet: 10.7639,
-        squareYards: 1.19599,
-      },
-      squareFeet: {
-        squareMeters: 0.092903,
-        squareYards: 0.111111,
-      },
-      squareYards: {
-        squareMeters: 0.836127,
-        squareFeet: 9,
-      },
-    };
-  
-    if (conversions[fromUnit] && conversions[fromUnit][toUnit]) {
-      return value * conversions[fromUnit][toUnit];
-    } else {
-      return "Invalid units or conversion not supported";
-    }
+  if (!value || !fromUnit || !toUnit) return;
+  const conversions = {
+    squareMeters: {
+      squareFeet: 10.7639,
+      squareYards: 1.19599,
+    },
+    squareFeet: {
+      squareMeters: 0.092903,
+      squareYards: 0.111111,
+    },
+    squareYards: {
+      squareMeters: 0.836127,
+      squareFeet: 9,
+    },
+  };
+
+  if (conversions[fromUnit] && conversions[fromUnit][toUnit]) {
+    return value * conversions[fromUnit][toUnit];
+  } else {
+    return "Invalid units or conversion not supported";
   }
+}
