16ac2ff178957a8f0e6f13ea40d1af8000fba61f
RounakBytes <144313548+RounakBytes@users.noreply.github.com>
2024-01-03 17:09:33 +0530

    Rounakj/area of circle (#5)
    * Sqaure Done
    
    * Area of Circle Implemented
    
    * Bug Fixed
    
    ---------
    
    Co-authored-by: RounakJoshi09 <rounakjsh783@gmail.com>

--- index.js
+++ index.js
@@ -21,18 +21,25 @@ const divide = (x, y) => {
 
   return x / y;
 };
+const square = (x) => {
+  return x * x;
+};
+
+const areaOfCircle = (radius) => {
+  return Math.PI * square(radius);
+};
 
 function calculateFactorial(number) {
-    console.log(url);
-    if (number === 0 || number === 1) {
-        return 1;
-    } else {
-        let factorial = 1;
-        for (let i = 2; i <= number; i++) {
-            factorial *= i;
-        }
-        return factorial;
+  console.log(url);
+  if (number === 0 || number === 1) {
+    return 1;
+  } else {
+    let factorial = 1;
+    for (let i = 2; i <= number; i++) {
+      factorial *= i;
     }
+    return factorial;
+  }
 }
 
 console.log(url);
@@ -42,17 +49,26 @@ console.log(multiply(25, 25));
 console.log(divide(5, 10));
 
 const areaInSquareMeters = 100; // Replace with your input value
-const convertedAreaInSquareFeet = convertArea(areaInSquareMeters, 'squareMeters', 'squareFeet');
-const convertedAreaInSquareYards = convertArea(areaInSquareMeters, 'squareMeters', 'squareYards');
+const convertedAreaInSquareFeet = convertArea(
+  areaInSquareMeters,
+  "squareMeters",
+  "squareFeet"
+);
+const convertedAreaInSquareYards = convertArea(
+  areaInSquareMeters,
+  "squareMeters",
+  "squareYards"
+);
 
-const celsiusToFahrenheit = convertTemperature(25, 'Celsius', 'Fahrenheit');
+const celsiusToFahrenheit = convertTemperature(25, "Celsius", "Fahrenheit");
 console.log(`25 Celsius is equal to ${celsiusToFahrenheit} Fahrenheit`);
 
-const kelvinToCelsius = convertTemperature(300, 'Kelvin', 'Celsius');
+const kelvinToCelsius = convertTemperature(300, "Kelvin", "Celsius");
 console.log(`300 Kelvin is equal to ${kelvinToCelsius} Celsius`);
 
-const fahrenheitToKelvin = convertTemperature(68, 'Fahrenheit', 'Kelvin');
+const fahrenheitToKelvin = convertTemperature(68, "Fahrenheit", "Kelvin");
 console.log(`68 Fahrenheit is equal to ${fahrenheitToKelvin} Kelvin`);
 console.log(calculateFactorial(50));
 
 console.log(decimalToBinary(50));
+console.log(areaOfCircle(50));
