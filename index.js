39478b08f0167250cb8647ca11e9b00b1ecb6f3b
RounakBytes <144313548+RounakBytes@users.noreply.github.com>
2024-01-03 15:47:02 +0530

    Divide Completed

--- index.js
+++ index.js
@@ -7,10 +7,19 @@ const substract = (x, y) => {
 };
 
 const multiply = (x, y) => {
-    return x * y;
-  }
-  
+  return x * y;
+};
 
-console.log(add(5,10));
-console.log(substract(10,5))
+const divide = (x, y) => {
+  if (y === 0) {
+    console.log("Cannot divide by zero");
+    return;
+  }
+
+  return x / y;
+};
+
+console.log(add(5, 10));
+console.log(substract(10, 5));
 console.log(multiply(25, 25));
+console.log(divide(5, 10));
